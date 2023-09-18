5cead0d6de570603296c4d51259a8fccdb0157dd
Le Huu Cuong <CuongLH@bkav.com>
2023-09-18 16:49:09 +0700

    add

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("hello world !"));
+app.get("/", (req, res) => res.send("hello world! wellcome to fullstack"));
 
 const PORT = 5000;
 
