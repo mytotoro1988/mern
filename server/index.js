7c3bb716196d9de4547600e9e2679d177180c15d
cuong <lehuucuong.k59@gmail.com>
2023-09-20 05:25:03 +0700

    add date from table

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const authRouter = require("./routes/auth");
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-learnit.y9role1.mongodb.net/mern-learnit?retryWrites=true&w=majority`
+    );
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 const app = express();
 
-app.get("/", (req, res) =>
-  res.send("hello world! wellcome to fullstack. commit from dev")
-);
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 const PORT = 5000;
 
